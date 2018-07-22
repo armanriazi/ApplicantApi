5d66295bdc03943425f3e131b9c7de89b97acdf7
armanriazi <armanriazi@hotmail.com>
2018-07-22 18:00:02 +0430

    cc

--- Core.ProjectInfrastructure/Persistence/RepositoryImplement/UnitOfWorkForPriceRepertory.cs
+++ Core.ProjectInfrastructure/Persistence/RepositoryImplement/UnitOfWorkForPriceRepertory.cs
@@ -6,6 +6,7 @@ using System.Text;
 
 namespace Core.ProjectInfrastructure.Persistence.RepositoryImplement
 {
+    //PriceRepertory
     public class UnitOfWorkForPriceRepertory : IUnitOfWorkForPriceRepertory
     {
         private IPriceRepertoryRepository _pricerepertoryRepository;
